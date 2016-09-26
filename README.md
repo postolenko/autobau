3735aa51ed0eda5d3cd0eecf8364001448d6b6f8
postolenko <denis.postolenko@yandex.ua>
2016-09-26 14:09:04 +0300

    Autobau

---
+++ README.md
@@ -0,0 +1,1 @@
+# http://postolenko.github.io/autobau/index.html
---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+    //   preload
+    // $(window).on('load', function () {
+
+    //     setTimeout(function() {
+
+    //         $(".preload-bg").fadeOut(500);
+
+    //     }, 700);
+  
+
+    // });
+
+
+    $(window).resize(function() {
+
+
+        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+        $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+    });
+
+
+    $(function() {
+
+        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+        $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+    });
+
+
+    $(function() {
+
+        var indexInput;
+
+        $(".search-form-box").click(function() {
+
+            indexInput = $(".search-form-box").index(this);
+
+            $(".search-form-box:eq("+ indexInput +") .search-form").addClass("active");
+
+        });
+
+    });
+
+
+});
